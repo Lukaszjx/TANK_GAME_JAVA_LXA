0bc1ec3f3ea6cc93dbb3cd3a3ed88bfb749b2761
Long Trần <longtran@Longs-MBP.lan>
2020-11-23 13:16:19 +0700

    Adding enum Direction, handle keyboard input for player's tank'

---
+++ TANK/src/Direction.java
@@ -0,0 +1,27 @@
+public enum Direction {
+    LEFT(-10, 0), RIGHT(10, 0), UP(0, -10), DOWN(0, 10);
+
+    private int deltaX = 0, deltaY = 0;
+
+    private Direction(int deltaX, int deltaY) {
+        this.deltaX = deltaX;
+        this.deltaY = deltaY;
+    }
+
+    public int getDeltaY() {
+        return deltaY;
+    }
+
+    public void setDeltaY(int deltaY) {
+        this.deltaY = deltaY;
+    }
+
+    public int getDeltaX() {
+        return deltaX;
+    }
+
+    public void setDeltaX(int deltaX) {
+        this.deltaX = deltaX;
+    }
+    
+}
