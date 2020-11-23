019bf26559e558dc1f424fc0deeb61247378573c
Long Trần <longtran@Longs-MacBook-Pro.local>
2020-11-23 09:37:42 +0700

    minor fixes with image resource

--- TANK/src/DrawTanks.java
+++ TANK/src/DrawTanks.java
@@ -5,20 +5,21 @@ import javax.swing.*;
 public class DrawTanks extends JPanel{  // DRAW IMAGE
     
     private ArrayList<Tank> list = new ArrayList<>();
-
     public DrawTanks() {
         Tank t1 = new Player(100, 200);
         Tank t2 = new Bot(200, 100);
         list.add(t1);
         list.add(t2);
-        setPreferredSize(new Dimension(100,200));
+
     }
     
     @Override
-    public void paint(Graphics g){
+    public void paintComponent(Graphics g){
         for (Tank i: list)
             g.drawImage(i.getImg(), i.getX(), i.getY(), null);
     }
-
+    public ArrayList<Tank> getList() {
+        return list;
+    }
 }
 
--- TANK/src/Player.java
+++ TANK/src/Player.java
@@ -7,11 +7,14 @@ public class Player extends Tank {
 
     public Player(int x, int y){
         super(x, y);
-        ImageIcon ii = new ImageIcon("src/TankD.gif");
+        ImageIcon ii = new ImageIcon("TANK/src/resource/PlayerTank.gif");
         img = ii.getImage();
     }
 
     public Image getImg() {
         return img;
+    }
+    public String toString() {
+        return "This is player tank";
     }
 }
