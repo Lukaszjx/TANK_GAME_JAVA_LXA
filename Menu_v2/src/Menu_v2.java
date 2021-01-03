21f6dea54372db234b9e50827d80345a86a3bf20
Lukaszjx <72375728+Lukaszjx@users.noreply.github.com>
2021-01-03 21:39:08 +0700

    Add files via upload

--- Menu_v2/src/Menu_v2.java
+++ Menu_v2/src/Menu_v2.java
@@ -1,8 +1,8 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
 
-public class Menu_v2 extends JFrame {
-    JButton button[];
+public class Menu_v2 extends JFrame  {
     
     public Menu_v2(){
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -16,7 +16,23 @@ public class Menu_v2 extends JFrame {
         //Adding buttons
         JButton b1 = new JButton("Start game");
         JButton b2= new JButton("Close game");
+        
+        //Adding action to the button
+        // b1.addActionListener(new ActionListener(){
+        //     @Override
+        //     public void actionPerformed(ActionEvent ae){
+        //         //Adding the file game here
+        //     }
+        // });
 
+        b2.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent ae){
+                System.exit(0);
+            }
+        });
+
+        
         b1.setAlignmentX(Component.CENTER_ALIGNMENT);
         b2.setAlignmentX(Component.CENTER_ALIGNMENT);
     
@@ -28,6 +44,14 @@ public class Menu_v2 extends JFrame {
     }
 
     public static void main(String[]args){
+        SplashScreen ss = new SplashScreen();
+        try{
+            Thread.sleep(5000);
+            ss.dispose();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
         new Menu_v2();
     }
+    
 }
